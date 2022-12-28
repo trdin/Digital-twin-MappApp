4daea90eeee8266d739c20ff7b7a9f7d9a751059
Peter <trdin.peter@gmail.com>
2022-12-28 15:40:58 +0100

    inital commit SMLTG-149

---
+++ desktop/src/eu/smltg/mapapp/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package eu.smltg.mapapp;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import eu.smltg.mapapp.DataVisualiserMap;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("data visualiser map");
+		new Lwjgl3Application(new DataVisualiserMap(), config);
+	}
+}
